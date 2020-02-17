cec9b7e82d5177cea00698d7c17affe92b1ef25a
Pham Van Lam <phamvanlam1991@gmail.com>
2020-02-17 22:21:56 +0700

    first basic table

---
+++ src/components/table/BasicTable.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h2>Basic Table</h2>
+    <el-table :data="tableData" border stripe>
+      <el-table-column
+        prop="date"
+        label="Date"
+        width="180"
+      ></el-table-column>
+      <el-table-column
+        prop="name"
+        label="Name"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="Address"
+        width="300">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BasicTable',
+  data () {
+    return {
+      tableData: [
+        {
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        },
+        {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        },
+        {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        },
+        {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>

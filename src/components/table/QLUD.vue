630f0fd30a489d392f2dce46d2d6b712f7d89ed5
kennylam91 <phamvanlam1991@gmail.com>
2020-02-18 08:57:52 +0700

    get data from qlduData

--- src/components/table/QLUD.vue
+++ src/components/table/QLUD.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
        <h3>Quan ly ung dung</h3>
+			 {{data}}
     </div>
 </template>
 <script>
+import { qludData } from '@/mock/QLUDMock'
 export default {
   name: 'QLUD',
   data () {
     return {
-
+      data: qludData
     }
   }
 }
---
+++ src/mock/QLUDMock.js
@@ -0,0 +1,8 @@
+export const qludData = [
+  { appCode: '1', appName: 'a', creator: 'A' },
+  { appCode: '2', appName: 'b', creator: 'B' },
+  { appCode: '3', appName: 'c', creator: 'C' },
+  { appCode: '4', appName: 'd', creator: 'D' },
+  { appCode: '5', appName: 'e', creator: 'E' },
+  { appCode: '6', appName: 'f', creator: 'F' }
+]

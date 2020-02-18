dc60026df5a481ed8ba4540d0b6a1a7864d1f379
kennylam91 <phamvanlam1991@gmail.com>
2020-02-18 11:54:41 +0700

    mock data: ok

--- src/components/table/QLUD.vue
+++ src/components/table/QLUD.vue
@@ -15,11 +15,11 @@
             <i class="el-icon-search"></i> Search</el-button>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="">
+          <el-button type="primary">
             <i class="el-icon-edit"></i> Add App</el-button>
         </el-form-item>
         <el-form-item>
-          <el-button type="secondary" @click="">
+          <el-button type="secondary" >
             <i class="el-icon-refresh"></i>  Reload page</el-button>
         </el-form-item>
       </el-form>
@@ -58,7 +58,7 @@
           width=""
           align="center"
         >
-        <el-button type="danger" @click="">Delete</el-button>
+        <el-button type="danger" size="small" >Delete</el-button>
 
         </el-table-column>
       </el-table>
@@ -74,17 +74,25 @@
     </div>
 </template>
 <script>
-import { qludData } from '@/mock/QLUDMock'
+import '@/mock/QLUDMock'
+import axios from 'axios'
 export default {
   name: 'QLUD',
   data () {
     return {
-      dataTable: qludData,
+      dataTable: null,
       formSearch: {
         appCode: '',
         appName: ''
       }
     }
+  },
+  mounted () {
+    axios.get('data.json')
+      .then(res => {
+        console.log(res.data.list)
+        this.dataTable = res.data.list
+      })
   }
 }
 </script>
--- src/mock/QLUDMock.js
+++ src/mock/QLUDMock.js
@@ -1,8 +1,10 @@
-export const qludData = [
-  { appCode: '1', appName: 'a', creator: 'A' },
-  { appCode: '2', appName: 'b', creator: 'B' },
-  { appCode: '3', appName: 'c', creator: 'C' },
-  { appCode: '4', appName: 'd', creator: 'D' },
-  { appCode: '5', appName: 'e', creator: 'E' },
-  { appCode: '6', appName: 'f', creator: 'F' }
-]
+import { Random } from 'mockjs'
+
+var Mock = require('mockjs')
+export default Mock.mock(/\.json/, {
+  'list|30-50': [{
+    'appCode|+1': 1,
+    'appName': '@string',
+    'creator': '@string'
+  }]
+})

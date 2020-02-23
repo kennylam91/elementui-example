a6662508ee87888b813079dab6320754dec4e332
Pham Van Lam <phamvanlam1991@gmail.com>
2020-02-23 08:11:08 +0700

    add selection table basic

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import BasicTable from '@/components/table/BasicTable'
+import SelectionTable from '@/components/table/SelectionTable'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
       path: '/table',
       name: 'BasicTable',
       component: BasicTable
+    },
+    {
+      path: '/table/selection',
+      name: 'SelectionTable',
+      component: SelectionTable
     }
   ]
 })
